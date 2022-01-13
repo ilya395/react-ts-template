6dfa29cbc495694bd9c7a95b40414e6bd94a4a2c
AXAPTA\iefimenko <AXAPTA\iefimenko>
2022-01-13 16:58:33 +0300

    airbnb rules is working... oh :(

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   env: {
     browser: true,
   },
-  extends: ['airbnb', 'airbnb/hooks', 'react-app', 'prettier', 'prettier/react'],
+  extends: ['airbnb', 'airbnb/hooks', 'react-app', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -11,7 +11,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['prettier', 'prettier', 'react', '@typescript-eslint'],
+  plugins: ['prettier', 'react', '@typescript-eslint'],
   rules: {
     'react/prop-types': 'off',
     '@typescript-eslint/no-unused-vars': 'error', // смотрим за не использованными переменными
@@ -33,6 +33,16 @@ module.exports = {
     'no-shadow': 'off',
     'react-hooks/rules-of-hooks': 'warn', // Проверяем правила хуков -> лучше поставить error на будущее
     'react-hooks/exhaustive-deps': 'warn', // Проверяем зависимости эффекта
+    "react/function-component-definition": [
+      2,
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    'arrow-body-style': ['error', 'as-needed', {
+      requireReturnForObjectLiteral: false,
+    }],
   },
   settings: {
     'import/resolver': {
--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import "./styles/styles.scss";
 import cn from "classnames";
 import s from "./App.module.scss";
 
-const App: React.FC = () => {
-    return (
-        <div className={cn(s["main-title"])}>
-            Это функциональный компонент! лол!!
-        </div>
-    );
-}
+const App: React.FC = () => (
+  <div className={cn(s["main-title"])}>
+    Это функциональный компонент! лол!!
+  </div>
+);
 
 export default App;
